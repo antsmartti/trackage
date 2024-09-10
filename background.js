9b716ab970810f84941845878814cc275d3ad6d8
antsmartti <36409715+antsmartti@users.noreply.github.com>
2024-09-10 10:01:21 +0300

    Update background.js
    updated dpd regex and url

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ chrome.runtime.onInstalled.addListener(() => {
     const couriers = [
       { id: "omniva", title: "Omniva", url: "https://minuold.omniva.ee/track/" },
       { id: "itella", title: "Itella", url: "https://itella.ee/eraklient/saadetise-jalgimine/?trackingCode=" },
-      { id: "dpd", title: "DPD", url: "https://www.dpdgroup.com/ee/mydpd/my-parcels/incoming?parcelNumber=" },
+      { id: "dpd", title: "DPD", url: "https://www.dpdgroup.com/ee/mydpd/my-parcels/track?lang=ee_ee&parcelNumber=" },
       { id: "venipak", title: "Venipak", url: "https://www.venipak.com/tracking/?id=" }
     ];
   
@@ -29,7 +29,7 @@ chrome.runtime.onInstalled.addListener(() => {
     const couriers = {
       omniva: `https://minuold.omniva.ee/track/${selectedText}`,
       itella: `https://itella.ee/eraklient/saadetise-jalgimine/?trackingCode=${selectedText}`,
-      dpd: `https://www.dpdgroup.com/ee/mydpd/my-parcels/incoming?parcelNumber=${selectedText}`,
+      dpd: `https://www.dpdgroup.com/ee/mydpd/my-parcels/track?lang=ee_ee&parcelNumber=${selectedText}`,
       venipak: `https://www.venipak.com/tracking/?id=${selectedText}`
     };
   
@@ -65,7 +65,7 @@ function identifyCourier(trackingNumber) {
   const couriers = [
     { id: "omniva", name: "Omniva", url: "https://minuold.omniva.ee/track/", pattern: /^[A-Z]{2}[0-9]{9}[A-Z]{2}$/i }, // Example: EE123456789EE
     { id: "itella", name: "Itella", url: "https://itella.ee/eraklient/saadetise-jalgimine/?trackingCode=", pattern: /^[A-Z]{2}[A-Z]{2}[0-9]{17}$/ }, // Example: JJFI63864910006517620
-    { id: "dpd", name: "DPD", url: "https://track.dpd.com/tracking?parcelNumber=", pattern: /^[0-9]{12}$/ }, // Example: 012345678901
+    { id: "dpd", name: "DPD", url: "https://www.dpdgroup.com/ee/mydpd/my-parcels/track?lang=ee_ee&parcelNumber=", pattern: /^[0-9]{14}$/ }, // Example: 01234567890123
     { id: "venipak", name: "Venipak", url: "https://www.venipak.com/tracking/?id=", pattern: /^[A-Z0-9]{8,12}$/i } // Example: ABCD123456
   ];
 
