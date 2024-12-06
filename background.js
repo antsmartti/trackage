5a19fc0f4375af0ad5b5d3e323bb2b14d024604f
antsmartti <36409715+antsmartti@users.noreply.github.com>
2024-12-06 09:42:51 +0200

    Update background.js
    updated Venipak url

--- background.js
+++ background.js
@@ -11,7 +11,7 @@ chrome.runtime.onInstalled.addListener(() => {
       { id: "omniva", title: "Omniva", url: "https://minuold.omniva.ee/track/" },
       { id: "itella", title: "Itella", url: "https://itella.ee/eraklient/saadetise-jalgimine/?trackingCode=" },
       { id: "dpd", title: "DPD", url: "https://www.dpdgroup.com/ee/mydpd/my-parcels/track?lang=ee_ee&parcelNumber=" },
-      { id: "venipak", title: "Venipak", url: "https://www.venipak.com/tracking/?id=" }
+      { id: "venipak", title: "Venipak", url: "https://venipak.com/ee/tracking/track/" }
     ];
   
     couriers.forEach(courier => {
@@ -30,7 +30,7 @@ chrome.runtime.onInstalled.addListener(() => {
       omniva: `https://minuold.omniva.ee/track/${selectedText}`,
       itella: `https://itella.ee/eraklient/saadetise-jalgimine/?trackingCode=${selectedText}`,
       dpd: `https://www.dpdgroup.com/ee/mydpd/my-parcels/track?lang=ee_ee&parcelNumber=${selectedText}`,
-      venipak: `https://www.venipak.com/tracking/?id=${selectedText}`
+      venipak: `https://venipak.com/ee/tracking/track/${selectedText}`
     };
   
     if (couriers[info.menuItemId]) {
@@ -66,7 +66,7 @@ function identifyCourier(trackingNumber) {
     { id: "omniva", name: "Omniva", url: "https://minuold.omniva.ee/track/", pattern: /^[A-Z]{2}[0-9]{9}[A-Z]{2}$/i }, // Example: EE123456789EE
     { id: "itella", name: "Itella", url: "https://itella.ee/eraklient/saadetise-jalgimine/?trackingCode=", pattern: /^[A-Z]{2}[A-Z]{2}[0-9]{17}$/ }, // Example: JJFI63864910006517620
     { id: "dpd", name: "DPD", url: "https://www.dpdgroup.com/ee/mydpd/my-parcels/track?lang=ee_ee&parcelNumber=", pattern: /^[0-9]{14}$/ }, // Example: 01234567890123
-    { id: "venipak", name: "Venipak", url: "https://www.venipak.com/tracking/?id=", pattern: /^[A-Z0-9]{8,12}$/i } // Example: ABCD123456
+    { id: "venipak", name: "Venipak", url: "https://venipak.com/ee/tracking/track/", pattern: /^[A-Z0-9]{8,12}$/i } // Example: ABCD123456
   ];
 
   // Try to match the tracking number against each courier's pattern
